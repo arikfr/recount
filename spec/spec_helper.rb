02f1ae7b13b0d41a07cb0e8993c2c810063e6050
Arik Fraimovich <arik@arikfr.com>
2011-09-18 23:53:52 +0300

    Updated spec_helper to check if it's running in TravisCI worker

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,36 +24,42 @@ module ReCount
 end
 
 RSpec.configure do |config|
-  ROOT_PATH = File.expand_path(File.dirname(__FILE__) + '/../')
-  REDIS_PID = "#{ROOT_PATH}/tmp/pids/redis-test.pid"
-  REDIS_CACHE_PATH = "#{ROOT_PATH}/tmp/cache/"
+  if ENV['TRAVIS']
+    config.before(:each) do
+      Redis.new.flushall
+    end
+  else
+    ROOT_PATH = File.expand_path(File.dirname(__FILE__) + '/../')
+    REDIS_PID = "#{ROOT_PATH}/tmp/pids/redis-test.pid"
+    REDIS_CACHE_PATH = "#{ROOT_PATH}/tmp/cache/"
 
-  config.before(:suite) do
-    redis_options = {
-      "daemonize"     => 'yes',
-      "pidfile"       => REDIS_PID,
-      "port"          => 9736,
-      "timeout"       => 300,
-      "save 900"      => 1,
-      "save 300"      => 1,
-      "save 60"       => 10000,
-      "dbfilename"    => "dump.rdb",
-      "dir"           => REDIS_CACHE_PATH,
-      "loglevel"      => "debug",
-      "logfile"       => "stdout",
-      "databases"     => 16
-    }.map { |k, v| "#{k} #{v}" }.join('\n')
-    `echo '#{redis_options}' | redis-server -`
-  end
+    config.before(:suite) do
+      redis_options = {
+        "daemonize"     => 'yes',
+        "pidfile"       => REDIS_PID,
+        "port"          => 9736,
+        "timeout"       => 300,
+        "save 900"      => 1,
+        "save 300"      => 1,
+        "save 60"       => 10000,
+        "dbfilename"    => "dump.rdb",
+        "dir"           => REDIS_CACHE_PATH,
+        "loglevel"      => "debug",
+        "logfile"       => "stdout",
+        "databases"     => 16
+      }.map { |k, v| "#{k} #{v}" }.join('\n')
+      `echo '#{redis_options}' | redis-server -`
+    end
 
-  config.before(:each) do
-    Redis.new(:port => 9736).flushall
-  end
+    config.before(:each) do
+      Redis.new(:port => 9736).flushall
+    end
 
-  config.after(:suite) do
-    %x{
-      cat #{REDIS_PID} | xargs kill -QUIT
-      rm -f #{REDIS_CACHE_PATH}dump.rdb
-    }
+    config.after(:suite) do
+      %x{
+        cat #{REDIS_PID} | xargs kill -QUIT
+        rm -f #{REDIS_CACHE_PATH}dump.rdb
+      }
+    end
   end
 end
